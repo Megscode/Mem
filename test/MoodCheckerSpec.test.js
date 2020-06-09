b320429db301f3901b26301e885ce32c845278e1
Student <emilywright212@gmail.com>
2020-06-09 11:46:01 +0100

    Max mood is set to 10

--- test/MoodCheckerSpec.test.js
+++ test/MoodCheckerSpec.test.js
@@ -15,4 +15,20 @@ describe("MoodChecker", function() {
     expect(mood.moodValue).toEqual(2);
   });
 
+  it("maximum mood value is 10", function() {
+    expect(mood.moodValue).toEqual(1)
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    mood.increaseMood()
+    expect(mood.moodValue).toEqual(10);
+  });
+
 });
