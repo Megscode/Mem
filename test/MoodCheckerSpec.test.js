54773d5baaee1caa51cb1ec65afdfbd0399d6158
Student <emilywright212@gmail.com>
2020-06-09 11:36:36 +0100

    Function increase mood exists

--- test/MoodCheckerSpec.test.js
+++ test/MoodCheckerSpec.test.js
@@ -9,4 +9,10 @@ describe("MoodChecker", function() {
     expect(mood.moodValue).toEqual(1);
   });
 
+  it("can increase mood", function() {
+    expect(mood.moodValue).toEqual(1)
+    mood.increaseMood()
+    expect(mood.moodValue).toEqual(2);
+  });
+
 });
