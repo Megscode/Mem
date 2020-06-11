76484410f5c09e29a94e05cf674fd9e0eb7ce500
Megan Hayes <megan.hayes@hotmail.co.uk>
2020-06-11 10:15:22 +0100

    test passing look through array 4

--- test/ActivitiesSpec.test.js
+++ test/ActivitiesSpec.test.js
@@ -8,4 +8,8 @@ describe("Activity", function() {
   it("has activities for mood 1", function() {
     expect(activity.mood1[0]).toEqual("Take a nap");
   });
+
+  it("should display a activity from array 4", function() {
+    expect(activity.mood4[1]).toEqual("Walk around the block");
+  });
 });
